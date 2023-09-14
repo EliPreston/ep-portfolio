b808460b8c153b1f31d6b95608012bdd473f5d02
Eli <eli@Elis-MacBook-Pro.local>
2023-09-14 17:53:31 -0400

    try deploy

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,28 +1,28 @@
 export default [
     {
         title: 'Project Name',
-        imgUrl: '/assets/Countach_Konji.jpg',
+        imgUrl: 'ep-portfolio/assets/Countach_Konji.jpg',
         description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat urna vel vestibulum elementum. Integer volutpat justo non mollis consectetur. Aenean ac volutpat tortor. In hac habitasse platea dictumst. Etiam ut ornare enim, sit amet sodales metus.',
         stack: ['HTML', 'CSS', 'JavaScript'],
         link: 'https://google.com'
     },
     {
         title: 'Project2 Name',
-        imgUrl: '/assets/Countach2_Konji.jpg',
+        imgUrl: 'ep-portfolio/assets/Countach2_Konji.jpg',
         description: 'This project was made using Python, the Requests library, and the Spotify developer API.',
         stack: ['Python', 'Requests', 'Spotify API'],
         link: 'https://google.com'
     },    
     {
         title: 'Project3 Name',
-        imgUrl: '/assets/NSX01.jpg',
+        imgUrl: 'ep-portfolio/assets/NSX01.jpg',
         description: 'This project was made using React, TailwindCSS, and the Vite build system.',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://google.com'
     },    
     {
         title: 'Project4 Name',
-        imgUrl: '/assets/NSX02.jpg',
+        imgUrl: 'ep-portfolio/assets/NSX02.jpg',
         description: 'This project was made using Python, the Pandas library, and the Numpy library.',
         stack: ['Python', 'Pandas', 'Numpy'],
         link: 'https://google.com'
