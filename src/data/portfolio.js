f2c25f060186c52b2ebcb95e80782b41cffaf7d8
Eli <eli@Elis-MacBook-Pro.local>
2023-09-14 23:34:00 -0400

    updating projects

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,30 +1,30 @@
 export default [
     {
-        title: 'Project Name',
-        imgUrl: 'assets/Countach_Konji.jpg',
-        description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat urna vel vestibulum elementum. Integer volutpat justo non mollis consectetur. Aenean ac volutpat tortor. In hac habitasse platea dictumst. Etiam ut ornare enim, sit amet sodales metus.',
-        stack: ['HTML', 'CSS', 'JavaScript'],
-        link: 'https://google.com'
+        title: 'Portfolio Web App',
+        imgUrl: 'assets/portfolio-stack-image.png',
+        description: 'The website you\'re viewing right now was built with React, TailwindCSS, Vite, as well as npm, and is hosted on Github Pages. I will continue to update this site as I complete more projects, big or small, and gain more experience.',
+        stack: ['React', 'Tailwind', 'Vite', 'Github Pages', 'npm'],
+        link: 'https://github.com/EliPreston/ep-portfolio'
     },
     {
-        title: 'Project2 Name',
-        imgUrl: 'assets/Countach2_Konji.jpg',
-        description: 'This project was made using Python, the Requests library, and the Spotify developer API.',
+        title: 'Spotify Playlist Scraper',
+        imgUrl: 'assets/spotify-developer.png',
+        description: 'Using Spotify\'s developer API, I created a Python script where I could enter anyone\'s user ID, send a request to the API, get back each of the user\'s playlists, and compile the data  into a JSON file.',
         stack: ['Python', 'Requests', 'Spotify API'],
-        link: 'https://google.com'
+        link: 'https://github.com/EliPreston/Python-Mini-Projects/tree/main/SpotifyAPI-Mini-Project'
     },    
     {
-        title: 'Project3 Name',
-        imgUrl: 'assets/NSX01.jpg',
-        description: 'This project was made using React, TailwindCSS, and the Vite build system.',
+        title: 'Java Audio App Simulator (text-based)',
+        imgUrl: 'assets/java-school-project.png',
+        description: 'Apart from the final exam, this project made up a notable portion of the grade for my Java class in school. The project was to create a command-line text-based simulated audio application where you could choose songs, audiobooks, or podcasts to \'download\', \'play\', and more.',
         stack: ['React', 'Tailwind', 'Vite'],
-        link: 'https://google.com'
+        link: 'https://github.com/EliPreston/ClassFiles/tree/main/W2023/CPS%20209/Assignments/A2-W2023'
     },    
     {
-        title: 'Project4 Name',
-        imgUrl: 'assets/NSX02.jpg',
-        description: 'This project was made using Python, the Pandas library, and the Numpy library.',
+        title: 'Visualizing Ethics of ChatGPT Survey',
+        imgUrl: 'assets/ethics-chatgpt.png',
+        description: 'As part of my Social Issues, Ethics and Professionalism in computer science course, I worked in a group with two other classmates to create a survey, distribute it to our peers, and then visualize the resulting data/answers using Python\'s Pandas and Numpy libraries.',
         stack: ['Python', 'Pandas', 'Numpy'],
-        link: 'https://google.com'
+        link: 'https://github.com/EliPreston/ClassFiles/tree/main/W2023/CPS%20412/Ethics%20Assignment'
     }
 ]
